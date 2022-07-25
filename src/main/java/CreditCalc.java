3afbd68c2fec147b9e953d795b30733db5f1f16d
Bair Arshaev <ars-bair@yandex.ru>
2022-07-25 23:29:57 +0300

    Основной код

--- src/main/java/CreditCalc.java
+++ src/main/java/CreditCalc.java
@@ -1,16 +1,29 @@
 public class CreditCalc {
+
+    private double creditSum;
+    private double creditTime;
+    private double creditRate;
+
     public CreditCalc(int creditSum, int creditTime, int creditRate) {
+        this.creditSum = creditSum;
+        this.creditTime = creditTime;
+        this.creditRate = creditRate;
+    }
+
+    public CreditCalc() {
+
     }
 
     public double getMonthlyPayment() {
-        return 0.0;
+        double r = creditRate / 1200; //Месячная процентная ставка
+        return creditSum * r * Math.pow(1 + r, creditTime) / (Math.pow(1 + r, creditTime) - 1);
     }
 
     public double totalCreditValue() {
-        return 0.0;
+        return creditTime * getMonthlyPayment();
     }
 
     public double creditOverpayment() {
-        return 0.0;
+        return totalCreditValue() - creditSum;
     }
 }
